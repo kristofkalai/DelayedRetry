c705a8d59fcab89642eaf8f7e383a80ed0d7820f
Kristóf Kálai <kristof.kalai@supercharge.io>
2023-10-22 00:36:48 +0200

    fix: update project

--- Sources/DelayedRetry/Publisher+Extensions.swift
+++ Sources/DelayedRetry/Publisher+Extensions.swift
@@ -1,6 +1,6 @@
 //
 //  Publisher+Extensions.swift
-//  
+//
 //
 //  Created by Kristóf Kálai on 2023. 08. 29..
 //
--- Sources/DelayedRetry/URLSession+Extensions.swift
+++ Sources/DelayedRetry/URLSession+Extensions.swift
@@ -1,6 +1,6 @@
 //
 //  URLSession+Extensions.swift
-//  
+//
 //
 //  Created by Kristóf Kálai on 2023. 08. 29..
 //
